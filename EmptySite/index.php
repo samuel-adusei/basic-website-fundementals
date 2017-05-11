d471048a69d592947252d67a6d9071a6f0fa3b46
samuel-adusei <samuel-adusei@hotmail.com>
2017-05-11 01:41:07 +0100

    setup

---
+++ EmptySite/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title> my website </title>
+    </head>
+    <body>
+        <h1> Sosa </h1>
+        
+    </body>
+</html>
